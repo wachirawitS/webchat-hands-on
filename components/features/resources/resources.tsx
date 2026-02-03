9a2127bcb6b3c0f24a9e689c4371ea7f8df5bb7f
wachirawitS <wachirawit.s@aware.co.th>
2026-02-04 05:24:56 +0700

    feat(resources): add size prop to buttons for consistent styling

--- components/features/resources/resources.tsx
+++ components/features/resources/resources.tsx
@@ -8,15 +8,16 @@ const Resources = () => {
   return (
     <div>
       <ButtonGroup>
-        <Button onClick={() => window.open('https://lin.ee/5VzqJZT', '_blank')} variant="outline">
+        <Button size="sm" onClick={() => window.open('https://lin.ee/5VzqJZT', '_blank')} variant="outline">
           <MessageCircle />
           Line OA
         </Button>
-        <Button variant="outline">
+        <Button size="sm" variant="outline">
           <Github fill="" />
           Git Hub
         </Button>
         <Button
+          size="sm"
           onClick={() =>
             window.open('https://www.figma.com/board/EDcQBSsiWUCCsC1hLiHSYL/Web-Chat-Handons?node-id=0-1&t=lK4sIYZ7akVeshAj-1', '_blank')
           }
